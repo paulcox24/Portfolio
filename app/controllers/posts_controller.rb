94938400cc7d771bc23b2b45152ef2c56b416b7a
Paul Cox <paulcox24@gmail.com>
2014-09-04 17:44:09 -0600

    Added comments, edit and delete for comments, seed of 10 comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
   # GET /posts/1.json
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 
   # GET /posts/new
@@ -30,7 +31,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
