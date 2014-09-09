5c7bdab69b463f50baf380636165055c6bf2990a
Paul Cox <paulcox24@gmail.com>
2014-09-08 22:16:35 -0600

    Changed db to postgresql

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 10.times do |counter|
   Post.create(
-    :title => "This is the title for post #{counter + 1}", 
-    :body => "This is the body for post #{counter + 1}", 
+    :title => "This is the title for #{ (counter + 1).ordinalize } post", 
+    :body => "This is the body for #{ (counter + 1).ordinalize } post", 
     :author => "Admin")
 end
