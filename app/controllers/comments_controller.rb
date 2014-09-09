8abc20e98c600ad8028e63e6edc6284a492137cd
Paul Cox <paulcox24@gmail.com>
2014-09-09 10:22:58 -0600

    comments create if statement

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,11 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-    @comment.save
+    if @comment.save
     redirect_to @post
+    else
+    redirect_to @post
+    end 
   end
 
   def edit
