db5969a396b34d0fd0e6586938c4c324755f0fd9
Paul Cox <paulcox24@gmail.com>
2014-09-15 21:51:08 -0600

    Add cocoon for comments and nested comment in show page of posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  accepts_nested_attributes_for :comments, :reject_if => :all_blank, :allow_destroy => true
   validates :title, :author, :body, presence: true  
 end
